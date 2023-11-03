25231d22c5091904b758e85b9c2958e5e7706f0f
Josy <camposjosybett@gmail.com>
2023-11-03 15:21:11 -0500

    reentrega desafio 3

--- src/app.js
+++ src/app.js
@@ -21,6 +21,11 @@ app.get('/products/:pid', async (req,res)=>{
         return res.status(400).send('Error, ingrese un argumento id numerico')
     }
     let resultado = await pm.getProductById(id)
+    if (Array.isArray(resultado) && !resultado.length) {
+        res.setHeader('Content-Type','application/json')
+        return res.status(404).json({'resultado': `No existe producto con id: ${id}`})
+    }
+
     res.setHeader('Content-Type','application/json')
     res.status(200).json({resultado})
 })
